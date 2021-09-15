681d6c1f984db2596a705e427a1973c47d5c2187
OlyaPinchuk <olha5959@gmail.com>
2021-09-16 01:21:46 +0300

    add UserNotesView

--- apps/notes/views.py
+++ apps/notes/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import CreateAPIView, ListAPIView, GenericAPIView, 
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import NoteSerializer
 from .models import NoteModel
@@ -16,3 +17,14 @@ class NoteListView(ListAPIView):
     queryset = NoteModel.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
+
+
+class UserNotes(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        db_notes = NoteModel.objects.filter(user_id=pk)
+        # db_notes = get_object_or_404(NoteModel.objects.all(), user_id=pk) # does not work
+        notes = NoteSerializer(db_notes, many=True).data
+        return Response(notes, status.HTTP_200_OK)
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserListView, UserUpToAdminView, UserUpdateProfileView, ChosenUserListView
+from ..notes.views import UserNotes
 
 urlpatterns = [
     path('', UserListView.as_view(), name='all_users'),
     path('/<int:pk>/to_admin', UserUpToAdminView.as_view(), name='up_to_admin'),
     path('/<int:pk>/profile', UserUpdateProfileView.as_view(), name='update_profile'),
     path('/<int:pk>', ChosenUserListView.as_view()),
-    # path('/<int:pk>/notes',)
+    path('/<int:pk>/notes', UserNotes.as_view())
 ]
