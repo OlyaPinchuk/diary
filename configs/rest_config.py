1749ca588e2f4a607053ce75cad2debdeab26e68
OlyaPinchuk <olha5959@gmail.com>
2021-11-19 23:34:11 +0200

    update rest config

--- configs/rest_config.py
+++ configs/rest_config.py
@@ -3,7 +3,7 @@ REST_FRAMEWORK = {
     'rest_framework_simplejwt.authentication.JWTAuthentication',
   ),
   'DEFAULT_PERMISSION_CLASSES': (
-    'rest_framework.permissions.AllowAny',
+    'rest_framework.permissions.IsAuthenticated',
   ),
   'DEFAULT_RENDERER_CLASSES': (
     'rest_framework.renderers.JSONRenderer',
